4f6a5ac09a238ee033b4be6b65023c8692535269
Debasmita Priyadarshini <80712354+debasmitaP10@users.noreply.github.com>
2021-03-30 00:23:44 +0530

    Changed

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 package firstHelloWorld;
 
 public class Hello {
-	  public static void main(String args[]){  
+	  public static void main(String[] args){  
 		     System.out.println("Hello Java"); 
 	  }
 }
